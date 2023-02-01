dafe92e8bef40cb4993854fcc7b8c6529113b070
Justin Yi <justinjyi96@gmail.com>
2023-02-01 13:55:50 -0500

    saving

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
-const editRoutes = require('./edit-routes');
+// const editRoutes = require('./edit-routes');
 const profileRoutes = require('./profile-routes');
 const jobRoutes = require('./jobRoutes')
 
 router.use('/users', userRoutes);
-router.use('/editProfile', editRoutes);
+// router.use('/editProfile', editRoutes);
 router.use('/profile', profileRoutes);
 router.use('/job', jobRoutes);
 
